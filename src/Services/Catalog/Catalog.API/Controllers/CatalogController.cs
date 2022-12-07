9d371d1d3e60cbeb4cc4ecad0ff9651e3e18a162
Ancel Miller <ancelclayton@gmail.com>
2022-12-06 22:12:11 -0700

    basket ms create

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,85 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository)); ;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProductById")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _productRepository.GetProductById(id);
+            if (product== null)
+            {
+                _logger.LogError($"Product with id: {id}, not found.");
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}", Name ="GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await _productRepository.GetProductByCategory(category);
+            return Ok(products);
+        }
+
+        [Route("[action]/{name}", Name = "GetProductByName")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string name)
+        {
+            var products = await _productRepository.GetProductByName(name);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.CreateProduct(product);
+            
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.UpdateProduct(product));
+        }
+
+        [HttpDelete("{id}", Name ="DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _productRepository.DeleteProductById(id));
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatelogContextSeed.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatelogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(CatelogFactory.getFactoryProducts());
+            }
+        }
+
+        
+    }
+}
